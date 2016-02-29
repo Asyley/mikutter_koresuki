d9ec862ce81bbdd7c960fccfe14b4e96d1f59bf8
Atsushi TAMANO <Asyley13@gmail.com>
2016-03-01 00:03:57 +0900

    Added files via upload

---
+++ mikutter_koresuki.rb
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+Plugin.create(:mikutter_koresuki) do
+  command(
+    :koresuki,
+    name: 'favRT＆これすき',
+    condition: -> _ { true },
+    visible: true,
+    role: :timeline
+  ) do |opt|
+
+    opt.messages.each { |m|
+          m.favorite
+          m.retweet
+
+          string = "これすき" + rand(0..99).to_s
+          Service.primary.update(:message => string)
+    }
+
+  end
+end
